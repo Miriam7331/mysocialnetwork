c2762ad0a8674341041fbed5911e5cfe1815bd1d
AlumGS <hello@example.com>
2024-09-24 18:10:20 +0100

    subiendo el repos

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->middleware(['auth', 'verified'])->name('contact');
+
+Route::get('/analytics', function () {
+    return view('analytics');
+})->middleware(['auth', 'verified'])->name('analytics');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
